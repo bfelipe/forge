c51e24e6b0873c2a9f6f018ffd622ad191425f00
Bruno Felipe <cs.felipebm@gmail.com>
2023-01-31 11:02:24 -0300

    add custom GOPATH to go development directory

--- forge.sh
+++ forge.sh
@@ -119,6 +119,7 @@ install_go() {
         sudo tar -C /usr/local -xzf /tmp/go$GO_VER.linux-amd64.tar.gz
         sudo sed -i "\$a export PATH=\$PATH:/usr/local/go/bin" /etc/profile
         sudo rm /tmp/go$GO_VER.linux-amd64.tar.gz
+        go env -w GOPATH=$DEV_PATH/dev/go
         $(go version)
     elif [ $GO_INSTALL_BOOL == "n" ]
     then
