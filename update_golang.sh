f0a2b0762e71a0325d2dae6e957c52565157079f
Bruno Felipe <cs.felipebm@gmail.com>
2023-02-02 16:48:29 -0300

    add update_golang module to quick update golang compiler to a given version

---
+++ update_golang.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+read -p "Please enter the Go version you wish to install: " GO_VER
+wget -c "https://go.dev/dl/go$GO_VER.linux-amd64.tar.gz" -P /tmp
+sudo tar -C /usr/local -xzf /tmp/go$GO_VER.linux-amd64.tar.gz
+sudo rm /tmp/go$GO_VER.linux-amd64.tar.gz
+go version
