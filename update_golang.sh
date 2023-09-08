d0deefeeefa07541795ce4ec1a9cf1d5ca328cb0
Bruno Felipe <cs.felipebm@gmail.com>
2023-09-08 13:16:01 -0300

    fix issue on updating go module where updating or downgrading still keep previous versions files.

--- update_golang.sh
+++ update_golang.sh
@@ -4,6 +4,7 @@ sudo apt update && sudo apt upgrade -y
 
 read -p "Please enter the Go version you wish to install: " GO_VER
 wget -c "https://go.dev/dl/go$GO_VER.linux-amd64.tar.gz" -P /tmp
+sudo rm -r /usr/local/go
 sudo tar -C /usr/local -xzf /tmp/go$GO_VER.linux-amd64.tar.gz
 sudo rm /tmp/go$GO_VER.linux-amd64.tar.gz
 go version
