c206cd3901a41ea42b119b72fae01bcff9f25f37
Bruno Felipe <cs.felipebm@gmail.com>
2024-04-30 11:10:53 -0300

    update docker enginer install

--- forge.sh
+++ forge.sh
@@ -194,18 +194,23 @@ sudo adduser $USER vboxusers
 clear
 
 # Docker
-sudo apt update
-sudo apt install --fix-broken -y
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo groupadd docker
-sudo usermod -aG docker $(whoami)
+sudo usermod -aG docker $USER
 newgrp docker << GROUP_SUBSHELL
+
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
 
 # Redis
 docker pull redis
